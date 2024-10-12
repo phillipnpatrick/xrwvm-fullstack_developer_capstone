803b8208f10aece68d810a7ea612e15a3b054075
Phillip Patrick <phillip.n.patrick@gmail.com>
2024-10-12 18:07:54 -0400

    Add CarMake and CarModel models

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'phillipnpatr-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost', 'phillipnpatr-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
-    'https://phillipnpatr-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://phillipnpatr-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
     'http://localhost:8000',  # Add localhost if you test locally
 ]
 
