3ccc89518b745815f846e8c35374fb96b91d8ea5
Phillip Patrick <phillip.n.patrick@gmail.com>
2024-10-13 10:08:35 -0600

    linter issues

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,9 +28,13 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'phillipnpatr-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = [
+    'localhost',
+    'phillipnpatr-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.' +
+    'proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
-    'https://phillipnpatr-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    'https://phillipnpatr-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01' +
+    '.proxy.cognitiveclass.ai',
     'http://localhost:8000',  # Add localhost if you test locally
 ]
 
@@ -98,7 +102,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.' +
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
